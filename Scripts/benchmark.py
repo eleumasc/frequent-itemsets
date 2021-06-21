b2287cbf430366d2418fafd58a87eb4ff8d0f60c
Samuele Casarin <eleumasc@gmail.com>
2021-06-21 09:56:28 +0000

    Update benchmark.py

--- Scripts/benchmark.py
+++ Scripts/benchmark.py
@@ -5,24 +5,63 @@ import subprocess
 import time
 
 
-NUM_THREADS = [2, 3, 4, 6, 8]  # [2, 4, 6, 8, 12]
+HARDWARE_CONCURRENCY = [8]
+NUM_THREADS = [2, 4, 6, 8, 10, 12]
 
 benchmark_info = [
     {
+        "versions": [True, True, True, True, True, True],
         "dataset": "chess",
         "filename": Path("datasets/chess.dat"),
         "ts_count": 3196,
-        "minsup_%": [20, 40, 60, 80],
-        "numThreads": NUM_THREADS,
-        "seqBound": sorted(list(dict.fromkeys(list(range(0, 75, 10)) + list(range(0, 30, 2)))))
+        "minsup_%": [10, 20, 30, 40, 50, 60, 70, 80, 90],
+        "numThreads": HARDWARE_CONCURRENCY,
+        "seqBound": [20]
     },
     {
+        "versions": [True, True, True, True, True, True],
         "dataset": "accidents",
         "filename": Path("datasets/accidents.dat"),
         "ts_count": 340183,
-        "minsup_%": [10, 20, 40, 60, 80],
+        "minsup_%": [10, 20, 30, 40, 50, 60, 70, 80, 90],
+        "numThreads": HARDWARE_CONCURRENCY,
+        "seqBound": [20]
+    },
+    {
+        "versions": [False, False, False, False, False, True],
+        "dataset": "chess",
+        "filename": Path("datasets/chess.dat"),
+        "ts_count": 3196,
+        "minsup_%": [15],
         "numThreads": NUM_THREADS,
-        "seqBound": sorted(list(dict.fromkeys(list(range(0, 486, 25)) + list(range(0, 30, 2)))))
+        "seqBound": sorted(list(dict.fromkeys(list(range(0, 76, 5)) + list(range(0, 31, 2)))))
+    },
+    {
+        "versions": [False, False, False, False, False, True],
+        "dataset": "accidents",
+        "filename": Path("datasets/accidents.dat"),
+        "ts_count": 340183,
+        "minsup_%": [2],
+        "numThreads": NUM_THREADS,
+        "seqBound": sorted(list(dict.fromkeys(list(range(0, 486, 25)) + list(range(0, 31, 2)))))
+    },
+    {
+        "versions": [False, False, True, False, False, False],
+        "dataset": "chess",
+        "filename": Path("datasets/chess.dat"),
+        "ts_count": 3196,
+        "minsup_%": [15],
+        "numThreads": [],
+        "seqBound": []
+    },
+    {
+        "versions": [False, False, True, False, False, False],
+        "dataset": "accidents",
+        "filename": Path("datasets/accidents.dat"),
+        "ts_count": 340183,
+        "minsup_%": [2],
+        "numThreads": [],
+        "seqBound": []
     }
 ]
 
@@ -44,7 +83,7 @@ def benchmark(args, n=3):
         for i in range(n):
             tic = time.time()
             subprocess.run(args=args, stdout=subprocess.DEVNULL,
-                           stderr=subprocess.DEVNULL, timeout=1200)  # timeout after 20 minutes
+                           stderr=subprocess.DEVNULL, timeout=600)  # timeout after 10 minutes
             toc = time.time()
             samples.append(toc - tic)
             print("#", end="", flush=True)
@@ -119,12 +158,18 @@ def benchmark_v5(info):
 
 def benchmark_all():
     for info in benchmark_info:
-        yield from benchmark_v1(info)
-        yield from benchmark_v2(info)
-        yield from benchmark_v25(info)
-        yield from benchmark_v3(info)
-        yield from benchmark_v4(info)
-        yield from benchmark_v5(info)
+        if info["versions"][0]:
+            yield from benchmark_v1(info)
+        if info["versions"][1]:
+            yield from benchmark_v2(info)
+        if info["versions"][2]:
+            yield from benchmark_v25(info)
+        if info["versions"][3]:
+            yield from benchmark_v3(info)
+        if info["versions"][4]:
+            yield from benchmark_v4(info)
+        if info["versions"][5]:
+            yield from benchmark_v5(info)
 
 
 def report_benchmark(bench, filename):
@@ -138,3 +183,4 @@ def report_benchmark(bench, filename):
 
 
 report_benchmark(benchmark_all(), "res_all.csv")
+
